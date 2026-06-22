03846cebbacaee18d8f1c46a90ec1708dd8b58ca
Dehterevich Daria <dehterevich.daria@gmail.com>
2026-06-22 12:55:33 +0300

    feat: add ClientType enum

--- LekuTrans/LekuTrans.Data/Models/ClientProfile.cs
+++ LekuTrans/LekuTrans.Data/Models/ClientProfile.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using LekuTrans.Data.Enums;
 
 namespace LekuTrans.Data.Models;
 
@@ -11,7 +12,7 @@ public class ClientProfile
     public long UserId { get; set; }
 
     [Column("type")]
-    public string Type { get; set; }
+    public ClientType Type { get; set; }
 
     [Column("is_active")]
     public bool IsActive { get; set; } = true;
