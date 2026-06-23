be0f56efee83ee6692ee5adaa4a7290f9e439104
Dehterevich Daria <dehterevich.daria@gmail.com>
2026-06-23 13:30:36 +0300

    feat: add generic IRepository and Repository

---
+++ LekuTrans/LekuTrans.Data/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekuTrans.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(long id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(long id);
+        void Save();
+    }
+
+}
---
+++ LekuTrans/LekuTrans.Data/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LekuTrans.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(long id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Create(T item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            _dbSet.Update(item);
+        }
+
+        public void Delete(long id)
+        {
+            T entity = _dbSet.Find(id);
+            if (entity != null)
+                _dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
